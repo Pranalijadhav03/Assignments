ed80075b699b410e4926dbc0a2c78415d2c42afc
Pranalijadhav03 <jadhav.pranali2403@gmail.com>
2022-02-13 15:42:46 +0530

    Qn.4 Write a program that reads 5 Random 2 Digit values , then find their sum and the average

---
+++ FindSumAndAvg.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -x
+
+totalsum=0
+avg=0
+for(( i=0;i<5;i++))
+do
+num=$((RANDOM%100))
+totalsum=$(($totalsum+$num))
+done
+avg=$(($totalsum/5))
+echo "total sum= "$totalsum
+echo "avg= "$avg
